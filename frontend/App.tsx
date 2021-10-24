22646303e87930562abd275cb2caf3a51b660270
Norman Kong <normankong@gmail.com>
2021-10-24 13:18:00 +0800

    Testing for Google Auth

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,23 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+// import useCachedResources from './hooks/useCachedResources';
+// import useColorScheme from './hooks/useColorScheme';
+// import Navigation from './navigation';
+
+// export default function App() {
+//   const isLoadingComplete = useCachedResources();
+//   const colorScheme = useColorScheme();
+
+//   if (!isLoadingComplete) {
+//     return null;
+//   } else {
+//     return (
+//       <SafeAreaProvider>
+//         <Navigation colorScheme={colorScheme} />
+//         <StatusBar />
+//       </SafeAreaProvider>
+//     );
+//   }
+// }
+
+
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button, View } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '306785828392-ekoicng2uojrgh9g4g5hljf45pamsiei.apps.googleusercontent.com',
+    iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+  });
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      }
+
+      console.log(response)
+  }, [response]);
+
+  return (
+    <View style={{paddingTop : 30}}>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+          }}
+      />
+    </View>
+  );
+
 }
