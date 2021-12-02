7f6339ba9ef84869f927ebc99d13422c868a28f4
Norman Kong <normankong@gmail.com>
2021-12-03 01:24:12 +0800

    Fix build issue.

--- frontend/screens/postlogin/finance/index.tsx
+++ frontend/screens/postlogin/finance/index.tsx
@@ -6,7 +6,6 @@ import { Icon } from 'react-native-elements'
 
 import { styles } from './styles'
 import apiHelper from '../../../common/apiHelper';
-import resHelper from '../../../common/resHelper';
 import neuHelper from '../../../common/neuHelper';
 import { ScrollView } from 'react-native-gesture-handler';
 import { LinearGradient, Stop, Defs } from 'react-native-svg'
@@ -29,7 +28,7 @@ export default function Screen(navigation) {
 
   const arrangeTxns = (data) => {
     const getDaysArray = (startDate, endDate) => {
-      let dates : String[]= []
+      let dates: String[] = []
       //to avoid modifying the original date
       const theDate = new Date(startDate)
       while (theDate < endDate) {
@@ -40,24 +39,25 @@ export default function Screen(navigation) {
     }
 
     const today = new Date()
-    const priorDate = new Date(new Date().setDate(today.getDate()-30))
+    const priorDate = new Date(new Date().setDate(today.getDate() - 30))
     const datesArr = getDaysArray(priorDate, today);
-    const graphDates = data.map(el=> ({
+    const graphDates = data.map(el => ({
       date: (new Date(el.txnDate)).toLocaleDateString("en-US"),
       amount: parseInt(el.txnAmount),
     }))
 
     const dailySpending = {}
-    datesArr.forEach(d => dailySpending[d] = 0)    
+    datesArr.forEach(d => dailySpending[d] = 0)
 
     graphDates.forEach(el => {
       if (el.date in dailySpending)
-      dailySpending[el.date] = dailySpending[el.date] + el.amount
+        dailySpending[el.date] = dailySpending[el.date] + el.amount
     })
-    const spendingArr : {date: string, amount: unknown}[] = []
+    const spendingArr: { date: string, amount: unknown }[] = []
     for (const [key, value] of Object.entries(dailySpending)) {
-      spendingArr.push({date: key, amount: value});
+      spendingArr.push({ date: key, amount: value });
     }
+
     setTxnsPerDay(spendingArr)
   }
 
@@ -78,44 +78,42 @@ export default function Screen(navigation) {
     getAccountTransaction();
   }, []);
 
-  const image = { uri: resHelper.account.acctTxnBackground };
-
   const Gradient = () => (
     <Defs key={'gradient'}>
-        <LinearGradient id={'gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
-            <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'}/>
-            <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'}/>
-        </LinearGradient>
+      <LinearGradient id={'gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
+        <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'} />
+        <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'} />
+      </LinearGradient>
     </Defs>
   )
 
-   return (
+  return (
     <View style={[styles.container]}>
 
       {/* Transaction History */}
       <View style={[styles.accountTransactionTable]}>
         {isLoading ?
           <PacmanIndicator color='white' size={60} /> :
-          
-            (accountTxnData.length == 0) ? <NoTransaction/> :
+
+          (accountTxnData.length == 0) ? <NoTransaction /> :
             (
               <>
-                <Text style={{ paddingLeft: 8, color: "white", fontSize: 16,}}>
+                <Text style={{ paddingLeft: 8, color: "white", fontSize: 16, }}>
                   Transaction History over last 30 days.
                 </Text>
                 <ScrollView style={[styles.graphBg]}>
                   <BarChart
-                    style={ { height: 200, paddingLeft: 40 } }
-                    data={ txnsPerDay.map(el=> el.amount) }
-                    contentInset={ { top: 20, bottom: 20 } }
-                    svg={ {
+                    style={{ height: 200, paddingLeft: 40 }}
+                    data={txnsPerDay.map(el => el.amount)}
+                    contentInset={{ top: 20, bottom: 20 }}
+                    svg={{
                       strokeWidth: 2,
                       fill: 'url(#gradient)',
-                    } }
-                    >
-                    <Grid/>
-                    <Gradient/>
-                  </BarChart>                  
+                    }}
+                  >
+                    <Grid />
+                    <Gradient />
+                  </BarChart>
                 </ScrollView>
                 <FlatList
                   refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
@@ -124,7 +122,7 @@ export default function Screen(navigation) {
                   renderItem={ItemView}
                 />
               </>)
-          
+
         }
       </View>
     </View>
@@ -133,8 +131,8 @@ export default function Screen(navigation) {
 
 const NoTransaction = () => {
   return (
-    <View style={{flex:5, backgroundColor:"white",justifyContent: "center",alignItems: "center", margin:5, borderRadius:3}}>
-      <Text style={{color:"blue"}}>No Transaction for this account for last 30 days</Text>
+    <View style={{ flex: 5, backgroundColor: "white", justifyContent: "center", alignItems: "center", margin: 5, borderRadius: 3 }}>
+      <Text style={{ color: "blue" }}>No Transaction for this account for last 30 days</Text>
     </View>
   )
 }
